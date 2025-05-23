207f76f1413d838bca7fcd302d72881a61ff92da
th3b0x <163587852+th3b0x2137@users.noreply.github.com>
2025-05-23 21:58:58 +0200

    Implemented bacis choise for what user wants to move

--- minecraft-added-files-manager/Program.cs
+++ minecraft-added-files-manager/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace minecraft_added_files_manager
+﻿using System;
+
+namespace minecraft_added_files_manager
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string fileK;
+            Console.WriteLine("Select the kind of file you want to import:");
+            Console.WriteLine("Mod     |     Texturepack     |     Shaders");
+            fileK = Convert.ToString(Console.ReadLine());
+            fileK.ToLower();
+            Console.WriteLine(fileK);
+
+
         }
     }
 }
