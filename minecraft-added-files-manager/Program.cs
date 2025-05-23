9683b28400cabb68fbe9f5bf1f64a567fc99c920
th3b0x <163587852+th3b0x2137@users.noreply.github.com>
2025-05-23 22:28:26 +0200

    added variables for folders path and looped program in caser of wrong answer

--- minecraft-added-files-manager/Program.cs
+++ minecraft-added-files-manager/Program.cs
@@ -7,12 +7,33 @@ namespace minecraft_added_files_manager
         static void Main(string[] args)
         {
             string fileK;
-            Console.WriteLine("Select the kind of file you want to import:");
-            Console.WriteLine("Mod     |     Texturepack     |     Shaders");
-            fileK = Convert.ToString(Console.ReadLine());
-            fileK.ToLower();
-            Console.WriteLine(fileK);
+            string fileN;
+            string fileDesM = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\.minecraft\mods";
+            string fileDesT = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\.minecraft\resourcepacks";
+            string fileDesS = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\.minecraft\shaderpacks";
 
+            do
+            {
+                Console.WriteLine("Select the kind of file you want to import:");
+                Console.WriteLine("Mod    |    Texturepack    |    Shaderpack");
+
+                fileK = Convert.ToString(Console.ReadLine());
+                fileK.ToLower();
+
+                if (fileK == "mod")
+                {
+                    Console.WriteLine("Enter the name of a file: ");
+                    fileN = Convert.ToString(Console.ReadLine());
+                }
+                if (fileK == "texturepack")
+                {
+
+                }
+                if (fileK == "shadespack")
+                {
+
+                }
+            }while(fileK != "mod" || fileK != "texturepack" || fileK != "shadespack");
 
         }
     }
